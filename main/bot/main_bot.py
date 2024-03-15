b7ea7f18e0360ae8f522a90176a388843bbc8f38
artiom_naumenko_1 <anaumenko484@bk.ru>
2024-03-15 16:14:32 +0300

    create logging

--- main/bot/main_bot.py
+++ main/bot/main_bot.py
@@ -1,9 +1,16 @@
+import telebot
 from telebot.async_telebot import AsyncTeleBot
 from django.conf import settings
 
 bot = AsyncTeleBot(settings.TG_API_KEY, parse_mode='HTML')
+telebot.logger.setLevel(settings.LOGLEVEL)
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 async def send_welcome(message):
-    await bot.reply_to(message, "!!!!")
+    await bot.reply_to(message, "Привет, друг!")
+
+
+@bot.message_handler(commands=['help'])
+async def send_help(message):
+    await bot.reply_to(message, "Чем тебе помочь?")
--- main/management/management/commands/run_bot.py
+++ main/management/management/commands/run_bot.py
@@ -1,11 +1,19 @@
-from django.core.management.base import BaseCommand, CommandError
+import logging
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
 import asyncio
 
 from bot.main_bot import bot
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
     help = 'Запускаем бота'
 
     def handle(self, *args, **options):
-        asyncio.run(bot.polling())
+        try:
+            asyncio.run(bot.infinity_polling(logger_level=settings.LOGLEVEL))
+        except Exception as err:
+            logger.error(f'Ошибка: {err}')
