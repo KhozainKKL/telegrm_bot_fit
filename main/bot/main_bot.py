e5f1c476fabc7cd790cda2d7e9395dca11ee2da8
artiom_naumenko_1 <anaumenko484@bk.ru>
2024-03-16 14:56:41 +0300

    create commands calendar and 1 sticker

--- main/bot/main_bot.py
+++ main/bot/main_bot.py
@@ -6,54 +6,103 @@ from telebot.async_telebot import AsyncTeleBot
 from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
 
 from bot.example_text import helper
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import calendar
 
 bot = AsyncTeleBot(settings.TG_API_KEY, parse_mode='HTML')
 telebot.logger.setLevel(settings.LOGLEVEL)
 
 
 @bot.message_handler(commands=['start'])
-async def calendar(message):
-    calendar, step = DetailedTelegramCalendar().build()
-    await bot.send_message(message.chat.id,
-                           f"Select {LSTEP[step]}",
-                           reply_markup=calendar)
+async def send_calendar(message):
+    now = datetime.datetime.now()
+    month = now.month
+    year = now.year
 
+    # Если текущий месяц последний в году, переключаемся на следующий год
+    if month == 12:
+        year += 1
 
-@bot.callback_query_handler(func=DetailedTelegramCalendar.func())
-async def cal(c):
-    result, key, step = DetailedTelegramCalendar().process(c.data)
-    if not result and key:
-        await bot.edit_message_text(f"Select {LSTEP[step]}",
-                                    c.message.chat.id,
-                                    c.message.message_id,
-                                    reply_markup=key)
-    elif result:
-        now = datetime.date.today()
-        then = datetime.datetime.strftime(result, '%Y-%m-%d')
+    markup = InlineKeyboardMarkup(row_width=3)
 
-        if now.month == 12:
-            if int(then.split('-')[1]) >= now.month - 9:
-                await bot.edit_message_text(f"Вы выбрали: {result}",
-                                            c.message.chat.id,
-                                            c.message.message_id)
-            else:
-                await bot.edit_message_text(f'Запись ведется только на первые 3 месяца следующего года.',
-                                            c.message.chat.id,
-                                            c.message.message_id)
+    # Генерируем кнопки для выбора месяца на русском языке
+    russian_month_names = [
+        "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+        "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+    ]
+    for i in range(3):
+        markup.add(InlineKeyboardButton(text=russian_month_names[(now.month + i - 1) % 12],
+                                        callback_data=f"month_{year}_{(month + i - 1) % 12 + 1}"))
 
-        elif int(then.split('-')[1]) >= now.month + 3 or not int(then.split('-')[0]) == now.year or (
-                int(then.split('-')[1]) < now.month or int(then.split('-')[0]) < now.year):
-            await bot.edit_message_text(f'Запись ведется только на 3 месяца до конца текущего года.',
-                                        c.message.chat.id,
-                                        c.message.message_id)
-        else:
-            await bot.edit_message_text(f"Вы выбрали: {result}",
-                                        c.message.chat.id,
-                                        c.message.message_id)
-            await bot.send_sticker(c.message.chat.id,
-                                   sticker='CAACAgIAAxkBAAELtpdl9WfB4snERAkVgZOph6nRzVHAYwACqQADFkJrCiSoJ_sldvhYNAQ')
+    await bot.send_message(message.chat.id, "Выберите месяц:", reply_markup=markup)
+
+    @bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'month')
+    async def choose_day(call):
+        year, month = map(int, call.data.split('_')[1:])
+        markup = InlineKeyboardMarkup()
+
+        num_days = calendar.monthrange(year, month)[1]
+
+        days_of_month = [day for day in range(1, num_days + 1)]
+
+        weeks = [days_of_month[i:i + 7] for i in range(0, len(days_of_month), 7)]
+
+        for week in weeks:
+            row = []
+            for day in week:
+                row.append(InlineKeyboardButton(text=str(day), callback_data=f"day_{year}_{month}_{day}"))
+            markup.row(*row)
+
+        markup.row(InlineKeyboardButton(text="Назад", callback_data="back_to_month"))
+
+        await bot.edit_message_text(f"Выберите день в {datetime.date(year, month, 1).strftime('%B')}:",
+                                    call.message.chat.id, call.message.message_id, reply_markup=markup)
+
+    @bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'day')
+    async def choose_time(call):
+        year, month, day = map(int, call.data.split('_')[1:])
+        markup = InlineKeyboardMarkup()
+
+        first_half_buttons = []
+        for hour in range(10, 15):
+            first_half_buttons.extend([
+                InlineKeyboardButton(text=f"{hour}:00", callback_data=f"time_{year}_{month}_{day}_{hour}_00"),
+            ])
+
+        markup.row(*first_half_buttons)
+
+        second_half_buttons = []
+        for hour in range(15, 21):
+            second_half_buttons.extend([
+                InlineKeyboardButton(text=f"{hour}:00", callback_data=f"time_{year}_{month}_{day}_{hour}_00"),
+            ])
+
+        markup.row(*second_half_buttons)
+
+        # Добавляем кнопку "Назад" для возврата к выбору дня
+        markup.row(InlineKeyboardButton(text="Назад", callback_data="back_to_month"))
+
+        await bot.edit_message_text(f"Выберите время в {datetime.date(year, month, day).strftime('%d.%m.%Y')}:",
+                                    call.message.chat.id, call.message.message_id, reply_markup=markup)
+
+    @bot.callback_query_handler(func=lambda call: call.data.split('_')[0] == 'time')
+    async def process_choice(call):
+        await bot.edit_message_text(f"<b>Вы выбрали дату и время: {'-'.join(call.data.split('_')[1:4])}, "
+                                    f"{':'.join(call.data.split('_')[4:6])}</b>\n Ждём Вас на тренировке.",
+                                    call.message.chat.id,
+                                    call.message.message_id)
+
+        await bot.send_sticker(call.message.chat.id,
+                               sticker='CAACAgIAAxkBAAELtpdl9WfB4snERAkVgZOph6nRzVHAYwACqQADFkJrCiSoJ_sldvhYNAQ')
+
+    @bot.callback_query_handler(func=lambda call: call.data == "back_to_month")
+    async def back_to_month(call):
+        await bot.edit_message_text("Выберите месяц:", call.message.chat.id, call.message.message_id,
+                                    reply_markup=markup)
 
 
 @bot.message_handler(func=lambda message: True)
 async def echo_message(message):
+    await bot.send_message(message.chat.id, f'Привет, {message.chat.first_name}')
     await bot.reply_to(message, helper)
