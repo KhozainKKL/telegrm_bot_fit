3d33cdd10e253a81543733597fbfde868835b2d6
artiom_naumenko_1 <anaumenko484@bk.ru>
2024-03-27 09:58:54 +0300

    change custom admin

--- main/main/urls.py
+++ main/main/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('grappelli/', include('grappelli.urls')),  # grappelli URLS
                   path('admin/', admin.site.urls),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
                                                                                            document_root=settings.MEDIA_ROOT)
--- main/main_table_admin/admin.py
+++ main/main_table_admin/admin.py
@@ -10,6 +10,24 @@ class TrainerFitModelAdmin(admin.ModelAdmin):
     list_display = ['date', 'lesson', 'trainer', 'number_of_recorded', 'check_canceled',
                     'check_canceled_description']
     list_filter = ('date', 'lesson', 'trainer')
+    readonly_fields = ('number_of_recorded',)
+    list_display_links = ['date', 'lesson', 'trainer', ]
+    # radio_fields = {'trainer': admin.HORIZONTAL}
+    autocomplete_fields = ["lesson"]
+    fieldsets = [
+        (
+            "Основная информация",
+            {
+                "fields": ["date", "lesson", "trainer", "number_of_recorded"],
+            },
+        ),
+        (
+            "Отмена занятия",
+            {
+                "fields": ["check_canceled", "check_canceled_description"],
+            },
+        ),
+    ]
 
 
 @admin.register(UserFitLesson)
@@ -17,3 +35,13 @@ class TrainerFitModelAdmin(admin.ModelAdmin):
     search_fields = ['user', 'lesson']
     list_display = ['user', 'lesson']
     list_filter = ('lesson', 'user')
+    list_display_links = ["user", "lesson"]
+
+    fieldsets = [
+            (
+                "Основная информация",
+                {
+                    "fields": ["user", "lesson",],
+                },
+            ),
+        ]
